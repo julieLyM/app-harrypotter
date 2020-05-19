bdb621f3d66611a152c026d266dcd89b95fe3b42
julieLyM <julielyminh@gmail.com>
2020-05-19 19:08:45 +0200

    test data service on react side

--- client/src/service/__mocks__/axios.js
+++ client/src/service/__mocks__/axios.js
@@ -1,2 +1,2 @@
 import mockAxios from 'jest-mock-axios';
-export default mockAxios.default;
+export default mockAxios;
--- client/src/service/data.test.js
+++ client/src/service/data.test.js
@@ -1,27 +1,36 @@
-const mockAxios = require('jest-mock-axios').default;
+import mockAxios from 'jest-mock-axios';
 
-import { getCharactersSrv, getHousesSrv } from './data';
+import { getCharactersSrv, getHousesSrv, getHouseById,getSpellsSrv } from './data';
 
 describe('test axios', () => {
   afterEach(() => {
     mockAxios.reset();
   });
 
-  // test('should call get data function with params page', async () => {
-  //   mockAxios.get.mockImplementationOnce(() =>
-  //     Promise.resolve({
-  //       data: { res: ['name'] },
-  //     })
-  //   );
+  test('should return the correct response', async () => {
+    mockAxios.get.mockReturnValue({
+      data: {
+        name: 'Harry Potter',
+      },
+    });
+    const result = await getCharactersSrv(1);
 
-  //   const res = await getCharactersSrv();
+    expect(result).toEqual({ name: 'Harry Potter' });
+  });
 
-  //   console.log(res);
-  //   expect(mockAxios.get).toHaveBeenCalledTimes(1);
-  //   expect(mockAxios.get).toHaveBeenCalledWith(`/api/v1/characters/`, {
-  //     params: { page: 1 },
-  //   });
-  // });
+
+  test('should use the parameter', () => {
+    const result = getCharactersSrv(1);
+
+    expect(mockAxios.get).toHaveBeenCalledWith(`/api/v1/characters/`,{params: {page: 1} });
+  });
+
+
+  test('should use the good verb', () => {
+    const result = getCharactersSrv(1);
+
+    expect(mockAxios.get).toHaveBeenCalled();
+  });
 
   test('should call get data function', async () => {
     mockAxios.get.mockImplementationOnce(() =>
@@ -29,11 +38,28 @@ describe('test axios', () => {
         data: { res: ['id'] },
       })
     );
-
     const res = await getHousesSrv();
-
-    console.log(res);
-
     expect(mockAxios.get).toHaveBeenCalledWith(`/api/v1/houses/`);
   });
+
+
+  test('should return the correct id houses', async () => {
+    mockAxios.get.mockReturnValue({
+      data: {
+        id: 1,
+      },
+    });
+    const result = await getHouseById(1);
+
+    expect(result).toEqual({id: 1 });
+  });
+
+
+  test('should use the parameter fetch spell', () => {
+    const result = getSpellsSrv(1);
+
+    expect(mockAxios.get).toHaveBeenCalledWith(`/api/v1/spells/`,{params: {page: 1} });
+  });
+
+
 });
